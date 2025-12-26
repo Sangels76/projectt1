23a769d19860a844756be1f68638a64fc4fb3aa4
Sangels76 <motovilovalexpro76@gmail.com>
2025-12-26 22:28:32 +0300

    commit

---
+++ SQL.py
@@ -0,0 +1,42 @@
+import sqlite3
+import random
+
+
+conn = sqlite3.connect('users.dn')
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    login VARCHAR(50),
+    password TEXT,
+    balance INTEGER(7)
+)
+""")
+
+
+def register_user():
+    print("Добро пожаловать в меню регистрации!")
+    login = input("Введите логин:")
+    password = input("Введите пароль:")
+    balance = round(random.uniform(100, 10000))
+    try:
+        cursor.execute("""
+        INSERT INTO users (login, password, balance)
+        VALUES (?,?,?)""", (login,password,balance)
+                       )
+        conn.commit()
+        print(f"Пользователь {login} успешно зарегистрирован!\n"
+              f"Ваш баланс: {balance}\n")
+
+    except sqlite3.IntegrityError:
+        print(f"Ошибка. Такой пользователь уже существует.")
+
+
+
+
+register_user()
+
+
+
+
